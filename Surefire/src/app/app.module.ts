159bf887f42bd814f442ae88779895206e784ffe
Nabila Kanwal <nk1915@live.com>
2017-10-08 15:09:21 +0500

    remove contact component

--- Surefire/src/app/app.module.ts
+++ Surefire/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ContactComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
